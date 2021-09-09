5fee011b3a9514453ef2baca590b94e7f129a067
Slobodan Moraca <moracaslobodan@gmail.com>
2021-09-09 21:36:14 +0200

    Added firebase backen server

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,6 +1,23 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupsPage (){
+    function addMeetupHandler(meetupData){
+        fetch('https://react-first-try-329c9-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers:{
+                    'Content-type': 'application/json'
+                }
+            }
+        );
+    }
+
     return(
-        <div>New Meetups Page</div>
+        <section>
+            <h1>Add New Meetup</h1>
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+        </section>
     );
 
 }
